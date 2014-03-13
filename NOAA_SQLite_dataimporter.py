69c9608afdaaac7eb3143a1ce3c001fb69a2d5d9
Morgan Ernest <morgan@weecology.org>
2014-03-13 14:12:48 -0400

    Cleaned up some variable and table names to be clearer and reduce redundancy

--- NOAA_SQLite_dataimporter.py
+++ NOAA_SQLite_dataimporter.py
@@ -10,10 +10,10 @@ data = pandas.read_csv("AllSanSimon.csv")
 con=dbapi.connect('SWweatherstations.sqlite')
 pandas.io.sql.write_frame(data, 'SanSimon', con, flavor='sqlite', if_exists='replace')
 
-data = pandas.io.sql.read_sql("SELECT YEAR, MONTH, AVG(TPCP/100.0) FROM SanSimon WHERE TPCP >= 0 GROUP BY YEAR, MONTH", con)
+data_ppt = pandas.io.sql.read_sql("SELECT YEAR, MONTH, AVG(TPCP/100.0) FROM SanSimon WHERE TPCP >= 0 GROUP BY YEAR, MONTH", con)
 rows = len(data)
 Site = ['SanSimon'] * rows
 data.insert(0, 'SITE', Site)
 data.rename(columns={'AVG(TPCP/100.0)':'AVG_TPCP_CM'}, inplace=True)
 
-pandas.io.sql.write_frame(data, 'SanSimon_summary', con, flavor='sqlite', if_exists='replace')
+pandas.io.sql.write_frame(data, 'Site_ppt_avgs', con, flavor='sqlite', if_exists='replace')
