4072b5079e8dbac2aef6877106fe2d69f1e063cd
Morgan Ernest <morgan@weecology.org>
2014-03-13 11:32:55 -0400

    Initial commit for code to take NOAA csv files, add to SQLite database and create summary tables stored in same SQLite database

---
+++ NOAA_SQLite_dataimporter.py
@@ -0,0 +1,19 @@
+"""To use this to import NOAA data, you lust open the original csv file and 
+split the Date column by hand into Year, Month, Flag"""
+import pandas
+import pandas.io.sql
+import sqlite3 as dbapi
+
+con=dbapi.connect('SWweatherstations.sqlite')
+
+data = pandas.read_csv("AllSanSimon.csv")
+con=dbapi.connect('SWweatherstations.sqlite')
+pandas.io.sql.write_frame(data, 'SanSimon', con, flavor='sqlite', if_exists='replace')
+
+data = pandas.io.sql.read_sql("SELECT YEAR, MONTH, AVG(TPCP/100.0) FROM SanSimon GROUP BY YEAR, MONTH", con)
+rows = len(data)
+Site = ['SanSimon'] * rows
+data.insert(0, 'SITE', Site)
+data.rename(columns={'AVG(TPCP/100.0)':'AVG_TPCP_IN'}, inplace=True)
+
+pandas.io.sql.write_frame(data, 'SanSimon_summary', con, flavor='sqlite', if_exists='replace')
