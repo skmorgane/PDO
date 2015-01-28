b6f2ed6999dbe2ba2aac731e7522e2f165553ed8
unknown <morgan@weecology.org>
2015-01-28 12:51:13 -0700

    initial commit of file to extract data for examining temporal scales of species composition project. This file is currently being modified from a file in the PortalPriceEqn repo. Uncommented sections work. All commented sections have not been modified yet

---
+++ PDO_spcomp.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import sqlalchemy
+import json
+
+
+######  MAIN CODE
+
+# Extracts subset of database from server that is needed for the project
+
+query_rats = """SELECT Rodents.mo, Rodents.dy, Rodents.yr, Rodents.period, 
+                Rodents.plot, Rodents.note1, Rodents.species, Rodents.wgt 
+                FROM Rodents JOIN SPECIES
+                ON Rodents.species=SPECIES.`New Code`
+                WHERE Rodents.plot IN ("2","4","8","11","12","14","17","22") 
+                AND (SPECIES.Rodent = 1)
+                AND (Rodents.note1 Is Null 
+                OR Rodents.note1 IN ("1", "2", "3", "6", "7", "10", 
+               "11", "12", "13", "14"))
+                """
+credentials = json.load(open("db_credentials.json", "r"))
+engine = sqlalchemy.create_engine('mysql+pymysql://morgan:{}@{}:{}/{}'.format(credentials['password'], credentials['host'], credentials['port'], credentials['database']))
+raw_data=pd.read_sql_query(query_rats, engine)
+
+# inserts species average mass for missing values & calculates 
+# individual energy use
+
+raw_data['wgt'] = raw_data[['species','wgt']].groupby("species").transform(lambda x: x.fillna(x.mean()))
+raw_data['energy'] = 5.69 * raw_data['wgt'] ** 0.75
+
+# imports trapping table, adds plot type info to table, calculates 
+# number of plots censused per period
+
+#Trapping_Table = pd.read_csv('Trapping_Table.csv')
+#Trapping_Table = pd.merge(Trapping_Table, plot_info, how='left', 
+                          #on='plot')
+#Trapping_Table['Type'] = Trapping_Table['Type Code'].map({'CO': 0, 
+                                                          #'LK': 1, 
+                                                          #'RE':2, 
+                                                          #'SK':1})
+#period_plot_count = Trapping_Table[['period', 'Type', 'sampled']].groupby(['period', 'Type']).sum()
+#period_plot_count.reset_index(inplace=True)
+
+## calculates mean energy use per species per treatment for each trapping 
+## session
+#treatment_sums = raw_data[['period', 'plot', 'Type', 'species', 'energy'
+                           #]].groupby(['period', 'Type', 'species']).sum()
+#treatment_sums.reset_index(inplace=True)
+#treatment_sums = pd.merge(treatment_sums, period_plot_count, how='left',
+                          #on=['period', 'Type'])
+#treatment_sums['average'] = treatment_sums['energy']/treatment_sums['sampled']
+
+## determines first Julian Date of trapping for each period, processes and merges
+## with data for export
+
+#JulianDate_for_period = Trapping_Table[['JulianDate', 
+                                        #'period']].groupby(['period']).min()
+#JulianDate_for_period['JulianDate'] = JulianDate_for_period['JulianDate'].astype(int)
+#JulianDate_for_period.reset_index(inplace=True)
+#treatment_sums = pd.merge(treatment_sums, JulianDate_for_period, how='left',
+                          #on=['period'])
+
+## formatting and output for analysis
+#treatment_data_export = treatment_sums.drop(['plot', 'energy', 'sampled'], 
+                                            #axis = 1)
+#treatment_data_export = treatment_data_export[(treatment_data_export['species'] != 'DX')
+                                              #& (treatment_data_export['species'] != 'PX')
+                                              #& (treatment_data_export['species'] != 'RX')
+                                              #& (treatment_data_export['species'] != 'OX')
+                                              #& (treatment_data_export['species'] != 'UR')
+                                              #& (treatment_data_export['species'] != 'SX')]
+                                              
+#treatment_data_export.to_csv("Portal_Rodents_PriceProject.csv")
