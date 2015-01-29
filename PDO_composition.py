23551f2a84ecd8c7834a50ecb3e3e61caeb5eecd
Morgan Ernest <morgan@weecology.org>
2015-01-29 10:30:29 -0700

    Changed import of table from portal package to import of file generated by separate program because this table needs to be added to the central database. Rewrote query_rats to download only records with valid species codes

--- PDO_composition.py
+++ PDO_composition.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import sqlalchemy
 import json
-import portal
 
 ###### FUNCTIONS
 def extract_data(query):
@@ -12,7 +11,7 @@ def extract_data(query):
     
     credentials = json.load(open("db_credentials.json", "r"))
     engine = sqlalchemy.create_engine('mysql+pymysql://morgan:{}@{}:{}/{}'.format(credentials['password'], credentials['host'], credentials['port'], credentials['database']))
-    data=pd.read_sql_query(query_rats, engine)
+    data = pd.read_sql_query(query, engine)
     return data
 
 def calculate_individual_energy(data):
@@ -26,35 +25,27 @@ def calculate_individual_energy(data):
 
 ######  MAIN CODE
 
-# Extracting control plot data for the entire time series
+# Extracting rodent records for known species from control plots for the entire 
+# time series
 
 query_rats = """SELECT Rodents.mo, Rodents.dy, Rodents.yr, Rodents.period, 
-                Rodents.plot, Rodents.note1, Rodents.species, Rodents.wgt 
+                Rodents.plot, Rodents.species, Rodents.wgt 
                 FROM Rodents JOIN SPECIES
                 ON Rodents.species=SPECIES.`New Code`
                 WHERE Rodents.plot IN ("2","4","8","11","12","14","17","22")
                 AND Rodents.period > 0
-                AND (SPECIES.Rodent = 1)
                 AND (Rodents.note1 Is Null 
                 OR Rodents.note1 IN ("1", "2", "3", "6", "7", "10", 
-               "11", "12", "13", "14"))
+                "11", "12", "13", "14"))
+                AND (SPECIES.Rodent = 1) AND (SPECIES.Unknown = 0)
                 """
 raw_data = extract_data(query_rats)
 raw_data = calculate_individual_energy(raw_data)
 
-Trapping_Table = portal.generate_trappingtable(raw_data)
-
 # imports trapping table, adds plot type info to table, calculates 
 # number of plots censused per period
-
-#Trapping_Table = pd.read_csv('Trapping_Table.csv')
-#Trapping_Table = pd.merge(Trapping_Table, plot_info, how='left', 
-                          #on='plot')
-#Trapping_Table['Type'] = Trapping_Table['Type Code'].map({'CO': 0, 
-                                                          #'LK': 1, 
-                                                          #'RE':2, 
-                                                          #'SK':1})
-#period_plot_count = Trapping_Table[['period', 'Type', 'sampled']].groupby(['period', 'Type']).sum()
+Trapping_Table = pd.read_csv('Trapping_Table.csv')
+period_plot_count = Trapping_Table[['period', 'sampled']].groupby(['period']).sum()
 #period_plot_count.reset_index(inplace=True)
 
 ## calculates mean energy use per species per treatment for each trapping 
