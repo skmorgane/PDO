eca744898b492e96a14a5b3aa936579ca73d392d
Morgan Ernest <morgan@weecology.org>
2015-09-16 09:11:04 -0400

    Changed the file path to read data file from subfolder. Changed the final year for the pdo index timeseries import and the plotting of the time series moving average.

--- PDO_code.R
+++ PDO_code.R
@@ -3,13 +3,13 @@ library(reshape)
 library(TTR)
 
 #imports PDO datafile and reshapes it for conversion to a time series object
-www = "PDO_clean.csv" 
+www = "./Data/PDO_clean.csv" 
 pdo.dat = read.csv(www, header=T)
 pdo.df = as.data.frame(pdo.dat)
 pdo.reshape = melt(pdo.dat, id=c("YEAR"))
 sort.pdoreshape = pdo.reshape[order(pdo.reshape$YEAR, pdo.reshape$variable),]
 PDO_vector = as.vector(sort.pdoreshape$value)
-pdo.ts = ts(PDO_vector, start=c(1900,1), end=c(2013,12), frequency=12)
+pdo.ts = ts(PDO_vector, start=c(1900,1), end=c(2014,12), frequency=12)
 
 plot_data_window = function(data, origin, terminal, xlabel, ylabel, title_plot){
   #takes time series data and specifics on time window, plot and axis labels and makes a graph
@@ -29,7 +29,7 @@ both_phases = plot_data_window(pdo.ts, c(1947,1), c(2013, 12), "Year", 'Importan
 
 
 pdo_win = SMA(pdo.ts,n=60) #smoothes time series by taking average over n months
-all_data = plot_data_window(pdo.ts, c(1900,1), c(2013, 12), "Year", 'Important Environment Condition', 
+all_data = plot_data_window(pdo.ts, c(1900,1), c(2014, 12), "Year", 'Important Environment Condition', 
                             "Monthly Environment Data (1900-2013)")
 lines(pdo_win,lwd=3, col='black')
 abline(v=1977, col='red', lwd=2)
